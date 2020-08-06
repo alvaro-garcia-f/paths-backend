e6a4f070680b77be323fa4b46c6b872494cf0a26
Álvaro García <alvaro.garcia.f@gmail.com>
2020-08-06 01:11:46 +0100

    results.controller - addAnswer() - lessonID and questionID converted to ObjectId before creation

--- api/controllers/result.controller.js
+++ api/controllers/result.controller.js
@@ -1,7 +1,10 @@
 const ResultModel = require('../models/results.model')
+const mongoose = require('mongoose')
 
 function addAnswer (req, res) {
   const data = req.body
+  data.lesson = mongoose.Types.ObjectId(data.lesson)
+  data.question = mongoose.Types.ObjectId(data.question)
   data.user = res.locals.user._id
 
   ResultModel
